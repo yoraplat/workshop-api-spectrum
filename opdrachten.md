35017d737ab0ade245a834bc58bf7d78a6655687
Yoram Platteeuw <yoram.platteeuw@telenet.be>
2024-12-05 17:29:03 +0100

    feat: add ai page

---
+++ opdrachten.md
@@ -0,0 +1,5 @@
+# Opdrachten
+- Correcte data velden toewijzen in de generateInfoTemplate() functie
+- Aantal vrije parkeerplaatsen berekenen
+- Infoborden aanvullen met een google maps link naar de respectievelijke parking
+- Adhv localstorage statussen van parkings bijhouden (bv )
---
+++ scripts/ai-script.js
@@ -0,0 +1,53 @@
+const spinner = $(".loading-state");
+const generatedImageEl = $("#generated-image");
+
+$(document).ready(function () {
+  $("#generate").click(function () {
+    const prompt = $("#prompt").val();
+    if (!prompt) {
+      alert("Geef een beschrijving van de afbeelding");
+      return;
+    }
+
+    const generationType = $("input[name='generationType']:checked").val();
+    const API_URL_FAST =
+      "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4";
+    const API_URL_SLOW =
+      "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev";
+    const API_TOKEN = "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";
+
+    $.ajax({
+      url: generationType !== "fast" ? API_URL_SLOW : API_URL_FAST,
+      type: "POST",
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+      contentType: "application/json",
+      data: JSON.stringify({
+        inputs: prompt,
+      }),
+      beforeSend: function () {
+        spinner.show();
+        generatedImageEl.hide();
+      },
+      success: function (response, status, xhr) {
+        const reader = new FileReader();
+        reader.onload = function (event) {
+          $("#generated-image").attr("src", event.target.result).show();
+        };
+        spinner.hide();
+        reader.readAsDataURL(response);
+      },
+      error: function (xhr, status, error) {
+        spinner.hide();
+        console.error("Error generating image:", error);
+        alert(
+          "Failed to generate the image. Please check the console for details."
+        );
+      },
+      xhrFields: {
+        responseType: "blob",
+      },
+    });
+  });
+});
