9f6ecea79516afa8bf5ada7702a660cf749c7263
Yoram Platteeuw <yoram.platteeuw@telenet.be>
2025-01-18 14:33:56 +0100

    clear student version

--- scripts/parking-script.js
+++ scripts/parking-script.js
@@ -1,8 +1,9 @@
 // Definieer een constante voor de API-URL waar de parkeerdata wordt opgehaald.
+// https://data.stad.gent/api/explore/v2.1/catalog/datasets/bezetting-parkeergarages-real-time/records
 
-// Definieer een constante om de spinner te kunnen selecteren
+// Definieer een constante om de spinner te kunnen selecteren adhv jquery
 
-// Definieer een constante om de container te selecteren waarin alle parking items geplaatst zullen worden
+// Definieer een constante om de container te selecteren (jquery) waarin alle parking items geplaatst zullen worden
 
 /** 
  * Schrijf een function om data op te halen via de API
